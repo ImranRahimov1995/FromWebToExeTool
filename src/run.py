aee73ff3119a0e75f18756b191fed8741fc0cf45
ImranRahimov1995 <imash.odessahouse@gmail.com>
2023-01-30 23:54:09 +0400

    optimize

--- src/run.py
+++ src/run.py
@@ -1,8 +1,14 @@
+import os
+from pathlib import Path
 from exemaker import ExeMaker, Settings
 
+
+BASE_DIR = str(Path(__file__).resolve().parent.parent)
+
+
 settings = Settings(
-    nativefier_dir="/home/impro/nativefier-apps/",
-    zip_dir="/home/impro/PycharmProjects/real_work/onWork/FromWebToExeTool/zips/",
+    nativefier_dir=os.path.join(BASE_DIR, 'nativefier-apps'),
+    zip_dir=os.path.join(BASE_DIR, 'zips'),
 )
 
 maker = ExeMaker(
@@ -12,16 +18,16 @@ maker = ExeMaker(
     config=settings,
 )
 
-maker2 = ExeMaker(
-    url="https://kinokong.org/36851-na-nozhah-1-6-sezon.html",
-    os_type="windows",
-    convert_mode="docker",
-    config=settings,
-)
+# maker2 = ExeMaker(
+#     url="https://kinokong.org/36851-na-nozhah-1-6-sezon.html",
+#     os_type="windows",
+#     convert_mode="docker",
+#     config=settings,
+# )
 
-maker3 = ExeMaker(
-    url="https://ubunlog.com/ru/pigz-%D1%81%D0%B6%D0%B8%D0%BC%D0%B0%D0%B5%D1%82-%D1%84%D0%B0%D0%B9%D0%BB%D1%8B-%D1%81-%D1%82%D0%B5%D1%80%D0%BC%D0%B8%D0%BD%D0%B0%D0%BB%D0%B0/",
-    os_type="mac",
-    convert_mode="npm",
-    config=settings,
-)
+# maker3 = ExeMaker(
+#     url="https://ubunlog.com/ru/pigz-%D1%81%D0%B6%D0%B8%D0%BC%D0%B0%D0%B5%D1%82-%D1%84%D0%B0%D0%B9%D0%BB%D1%8B-%D1%81-%D1%82%D0%B5%D1%80%D0%BC%D0%B8%D0%BD%D0%B0%D0%BB%D0%B0/",
+#     os_type="mac",
+#     convert_mode="npm",
+#     config=settings,
+# )
