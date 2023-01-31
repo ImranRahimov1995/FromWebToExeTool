78cc94dea9f3edf9dcae8002aa92d3814ea0cff9
ImranRahimov1995 <imash.odessahouse@gmail.com>
2023-01-31 11:33:52 +0400

    additional files

---
+++ exemaker.sh
@@ -0,0 +1,40 @@
+sudo apt update
+
+# install node js
+cd ~
+sudo apt-get install -y yarn
+curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install -y nodejs
+node -v
+
+sudo npm install -g nativefier
+
+
+# install docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker
+
+
+
+# use docker without sudo
+sudo usermod -aG docker ${USER}
+groups
+sudo usermod -aG docker username
+
+#docker-compose
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+# wine stable Wine is needed for generating windows exe on linux:
+sudo apt-get install -y wine-stable
+
+
